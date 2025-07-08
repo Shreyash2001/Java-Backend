0d90b655beec61f7ffe46e4d6754a013a905b57e
Shreyash2001 <shreyashkatare21@gmail.com>
2025-07-09 01:31:46 +0530

    breaking code into microservices

---
+++ auth-service/src/main/java/org/example/entity/User.java
@@ -0,0 +1,17 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long id;
+    private String username;
+    private String password;
+}
---
+++ config-server/src/main/java/org/example/ConfigServerApplication.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringBootApplication.run(ConfigServerApplication.class, args);
+    }
+}
